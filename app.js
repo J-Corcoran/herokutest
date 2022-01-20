7d1bb4a61c4204f04149c217a71f33992fd23413
Joe Corcoran <90690530+J-Corcoran@users.noreply.github.com>
2022-01-20 09:30:59 +0000

    simple server deploy

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
-var express = require('express');
-const app = express();
-const port = process.env.PORT || 3000; //sets to provided port or 3000 (i.e. if running locally)
+// create an express app
+const express = require("express")
+const app = express()
 
-
+// use the express-static middleware
 app.use(express.static("public"))
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-});
+// define the first route
+app.get("/joe", (req, res) => {
+
+  res.send("<h1>Hello World!</h1>")
+})
+
+app.get("/joe2", (req, res) => {
+
+    res.send("<h1>Hello World2!</h1>")
+  })
+  
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
 
 
