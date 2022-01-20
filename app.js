297ef4285bfeb9b7d5098f9417dcc6638004cbe2
Joe Corcoran <90690530+J-Corcoran@users.noreply.github.com>
2022-01-20 09:29:56 +0000

    simple server deploy

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var express = require('express');
 const app = express();
 const port = process.env.PORT || 3000; //sets to provided port or 3000 (i.e. if running locally)
 
+
+app.use(express.static("public"))
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
 
-app.use(express.static('public'));
 
